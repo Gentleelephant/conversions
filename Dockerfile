d9bc550e1fdf8bc36fd3209f4dd49b2d1aeffbb1
dehaocheng <dehaocheng@kubesphere.io>
2024-04-17 16:21:46 +0800

    conversion
    Signed-off-by: dehaocheng <dehaocheng@kubesphere.io>

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+# Build the manager binary
+FROM golang:1.20 as builder
+
+ENV GO111MODULE=on \
+    CGO_ENABLED=0 \
+    GOOS=linux \
+    GOARCH=amd64 \
+	GOPROXY="https://goproxy.cn,direct"
+
+# 移动到工作目录：/home/www/goWebBlog 这个目录 是你项目代码 放在linux上
+# 这是我的代码跟目录
+# 你们得修改成自己的
+WORKDIR /dist
+COPY conversion.go conversion.go
+COPY constants/ constants/
+COPY utils/ utils/
+COPY go.mod .
+COPY go.sum .
+RUN go mod download
+
+# 将代码复制到容器中
+
+# 将我们的代码编译成二进制可执行文件  可执行文件名为 app
+RUN CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -a -o app .
+
+
+# 启动容器时运行的命令
+CMD ["./app"]
--- conversion.go
+++ conversion.go
@@ -1,26 +1,31 @@
 package conversions
 
-
 import (
-	"encoding/json"
+	"conversions/template"
+	"conversions/utils"
 	"fmt"
+	"github.com/go-kit/kit/log/level"
 	_ "k8s.io/api/core/v1"
-	_ "log"
+	 "log"
 	"net/http"
 )
 
-// 定义Alertmanager告警的结构
-type AlertmanagerAlert struct {
-	Status string `json:"status"`
-	Labels struct {
-		Alertname string `json:"alertname"`
-		Severity  string `json:"severity"`
-		// 其他告警标签
-	} `json:"labels"`
-	Annotations struct {
-		Summary string `json:"summary"`
-		// 其他注释
-	} `json:"annotations"`
+type AlertNotify struct {
+	emails []string
+	mobiles []string
+	wechats []string
+}
+
+type Lightning struct {
+	alertName string
+	alertDesc string
+	alertLevel string
+	alertStatus string
+	applyType string
+	alertNotify AlertNotify
+	alertKey string
+	alertId string
+	alertMsgId string
 }
 
 // 处理接收到的Alertmanager告警
@@ -30,28 +35,20 @@ func alertHandler(w http.ResponseWriter, r *http.Request) {
 	// https://github.com/prometheus/alertmanager/blob/master/template/template.go#L231
 	data := template.Data{}
 	if err := utils.JsonDecode(r.Body, &data); err != nil {
-		h.handle(w, &response{http.StatusBadRequest, err.Error()})
+		log.Printf(err.Error())
 		return
 	}
 	// 读取请求体
-	var alert AlertmanagerAlert
-	err := json.NewDecoder(r.Body).Decode(&alert)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
+	 sendLightning := make([]Lightning, len(data.Alerts))
 
-	// 转换成萤火虫的格式
-	fireflyAlert := map[string]interface{}{
-		"status":       alert.Status,
-		"alertname":    alert.Labels.Alertname,
-		"severity":     alert.Labels.Severity,
-		"summary":      alert.Annotations.Summary,
-		// 其他字段转换
+	for i, alert := range data.Alerts {
+		sendLightning[i].alertName = alert.Labels["alertname"]
+		alert.ID = utils.Hash(alert)
+
 	}
 
 	// 发送给萤火虫
-	sendToFirefly(fireflyAlert)
+	//sendToFirefly(sendLightning)
 
 	// 返回成功响应
 	w.WriteHeader(http.StatusOK)
@@ -59,15 +56,15 @@ func alertHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // 发送数据给萤火虫
-func sendToFirefly(data map[string]interface{}) {
-	// 实现发送逻辑，将数据发送给萤火虫
-	// 例如使用HTTP POST请求发送数据给萤火虫的API
-}
+//func sendToFirefly(data map[string]interface{}) {
+//	// 实现发送逻辑，将数据发送给萤火虫
+//	// 例如使用HTTP POST请求发送数据给萤火虫的API
+//}
 
 func main() {
 	// 注册处理程序
 	http.HandleFunc("/alert", alertHandler)
- event.
+
 	// 启动HTTP服务器
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
